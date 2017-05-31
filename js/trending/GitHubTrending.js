02b3a34355d929a6d4b192789115d9e3460e51c9
gangqiang <gangqiang.duan@idsuipai.com>
2017-05-31 14:32:28 +0800

    格式化代码

--- js/trending/GitHubTrending.js
+++ js/trending/GitHubTrending.js
@@ -1,29 +1,28 @@
 import TrendingUtil from './TrendingUtil';
-
+/**
+ * 抓取GitHub的Trending页面的数据
+ */
 export default class GitHubTrending {
-  GitHubTrending(){//Singleton pattern
-    if (typeof GitHubTrending.instance==='object') {
-      return GitHubTrending.instance;
-    }
-    GitHubTrending.instance=this;
-  }
-  fetchTrending(url){
-    return new Promise((resolve,reject)=>{
-      fetch(url)
-      .then((response)=>response.text())
-      .catch((error)=>{
-        reject(error);
-        console.log(error);
-      }).then((responseData)=>{
-        try {
-          resolve(TrendingUtil.htmlToRepo(responseData));
-        } catch (e) {
-          reject(e);
+    GitHubTrending() {//Singleton pattern
+        if (typeof GitHubTrending.instance === 'object') {
+            return GitHubTrending.instance;
         }
-      }).done();
-    });
-  }
+        GitHubTrending.instance = this;
+    }
 
-
-
+    fetchTrending(url) {
+        return new Promise((resolve, reject) => {
+            fetch(url)
+                .then((response) => response.text())
+                .catch((error) => {
+                    reject(error);
+                }).then((responseData) => {
+                try {
+                    resolve(TrendingUtil.htmlToRepo(responseData));
+                } catch (e) {
+                    reject(e);
+                }
+            }).done();
+        });
+    }
 }
--- js/trending/StringUtil.js
+++ js/trending/StringUtil.js
@@ -1,11 +1,11 @@
 export default class StringUtil {
 
-  static trim( text ){
-    if (typeof(text) === "string")  {
-      return text.replace(/^\s*|\s*$/g, "");
+    static trim(text) {
+        if (typeof(text) === "string") {
+            return text.replace(/^\s*|\s*$/g, "");
+        }
+        else {
+            return text;
+        }
     }
-    else{
-      return text;
-    }
-  }
 }
